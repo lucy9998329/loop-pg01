97dc36fb4fba6f31d07a7214f0b6392bca89aab6
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-08-12 11:41:43 +1200

    Inicial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loops_pg01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
